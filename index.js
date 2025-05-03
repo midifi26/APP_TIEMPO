e9a3d1ec8c59ae5d78e8bdd881b1ade1e779648b
Michelle Alejandra Díaz Figueroa <midifi26@gmail.com>
2025-05-03 14:16:19 +0200

    Agregada la vista

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const request = require('request');
+// const request = require('request');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const argv = require('yargs').argv;
-const apiKey = process.env.API_KEY;
+// const argv = require('yargs').argv;
+// const apiKey = process.env.API_KEY;
 
-let city = argv.c || 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+// let city = argv.c || 'portland';
+// //let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
 
 
-request(url, function (err, response, body) {
-  if(err){
-    console.log('error:', error);
-  } else {
-    let weather = JSON.parse(body)
-let message = `It's ${weather.main.temp} degrees in
-               ${weather.name}!`;
-    console.log(message);
-  }
-});
+// request(url, function (err, response, body) {
+//   if(err){
+//     console.log('error:', error);
+//   } else {
+//     let weather = JSON.parse(body)
+// let message = `It's ${weather.main.temp} degrees in
+//                ${weather.name}!`;
+//     console.log(message);
+//   }
+// });
